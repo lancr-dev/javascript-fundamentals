cea9beab17a6a4632d07f1ffc84d932cc7cdcb3b
Lance <lanceandreiducante@gmail.com>
2026-09-17 21:34:46 +0800

    added switch statement

---
+++ switchStatement.js
@@ -0,0 +1,16 @@
+// Switch statement
+let course = 1;
+
+switch (course) {
+  case 1:
+    console.log('BSCS');
+    break;
+  case 2:
+    console.log('BSIT');
+    break;
+  case 3:
+    console.log('BSBA');
+    break;
+  default:
+    console.log('Not a Valid Number of course.');
+}
