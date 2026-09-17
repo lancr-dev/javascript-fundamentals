59dcf04a79d7b1a8a24bc64feb49d334fb9a3c0a
Lance <lanceandreiducante@gmail.com>
2026-09-17 16:11:41 +0800

    added array and methods

---
+++ arrays.js
@@ -0,0 +1,20 @@
+// JavaScript arrays and methods
+let names = ['Lance', 'John', 'Alice'];
+let numbers = [2, 4, 6, 8, 10];
+let mixedValues = ['Lance', 2, true];
+
+// How to determine array length
+let arrLength = names.length;
+console.log(arrLength);
+
+// How to read an array
+console.log(names[1]);
+
+// How to update array
+names[0] = 'Alex';
+console.log(names);
+
+// Deleting arrays
+names.pop(); // remove the last value
+names.shift(); // remove the first values
+names.slice(start, end);
