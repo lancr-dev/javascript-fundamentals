4a6384ac5de6847b62d72a6665473dab97d01b7f
Lance <lanceandreiducante@gmail.com>
2026-09-18 21:52:55 +0800

    added functions

---
+++ functions.js
@@ -0,0 +1,18 @@
+// function
+function greet() {
+  console.log('Hello World!');
+}
+
+// function with parameter
+function greetStudent(student) {
+  console.log(`Hello ${student}`);
+}
+
+greetStudent('Lance');
+
+// another example
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(10, 20));
