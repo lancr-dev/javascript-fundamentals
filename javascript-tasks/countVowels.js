d2fbf4c5a5f3c3a992de5e66a8490c9741d4706d
Lance <lanceandreiducante@gmail.com>
2026-09-22 10:59:34 +0800

    1 task done

---
+++ javascript-tasks/countVowels.js
@@ -0,0 +1,14 @@
+const countVowels = (str) => {
+  let count = 0;
+  const vowels = 'aeiouAEIOU';
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+console.log(countVowels('Sample Word'));
